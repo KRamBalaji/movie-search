8f0737815edb8fecef2b58404f6a070ecbbce016
KRamBalaji <krambalaji27@gmail.com>
2021-11-05 18:59:52 +0530

    managed state with react useState hook

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchMovies = () => {
+    
+    //states- input query, movies
+    const [query, setQuery] = useState('');
+    //create the state for movies, and update that state appropriate
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (e) => {
         e.preventDefault();
-        console.log("submitting");
-        
-        const query = "Jurassic Park";
         
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5b3eb7a7bf2c0e8ebd5a678044008818&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data);
+            setMovies(data.results);
         }catch(err){
             console.error(err);
         }
@@ -23,7 +25,9 @@ const SearchMovies = () => {
     <form className="form" onSubmit={searchMovies}>
         <label className="label" htmlFor="query">Movie Name</label>
         <input className="input" type="text" name="query"
-        placeholder="i.e. Jurassic Park"/>
+        placeholder="i.e. Jurassic Park"
+        value={query} onChange={(e) => setQuery(e.target.value)}
+        />
         <button className="button" type="submit">Search</button>
     </form>
     )
